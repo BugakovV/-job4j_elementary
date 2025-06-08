e65c9f24522a6e6e29894d7bc1cafc3c802133eb
BugakovV <bugakov-v@internet.ru>
2025-06-08 16:05:29 +0700

    1. Модульные тесты [#505277 #545698] Cl bot исправления

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -3,7 +3,6 @@ package ru.job4j.converter;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-
 class ConverterTest {
 
     @Test
@@ -18,7 +17,7 @@ class ConverterTest {
     @Test
     void  whenConvert200RblThen3dot3333Dollar() {
         float input = 200;
-        float expected =3.3333f;
+        float expected = 3.3333f;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
